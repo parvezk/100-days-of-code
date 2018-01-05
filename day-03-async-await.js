7f03b08603393452c73864096793541a827fde89
Parvez Kose <flamboyanz@gmail.com>
2018-01-05 01:59:11 -0500

    day 3 async await

--- day-03-async-await.js
+++ day-03-async-await.js
@@ -1,11 +1,13 @@
 /* 
 // Async/await 101
-// Async/await is a new way to write asynchronous code. 
-// Previous options for asynchronous code are callbacks and promises.
-// Async/await is, like promises, non blocking.
-// Async/await is actually built on top of promises
-// It cannot be used with plain callbacks or node callbacks.
-// Async/await makes asynchronous code look and behave a little more like synchronous code. This is where all its power lies.
+// - Async/await is a new way to write asynchronous code. 
+// - Previous options for asynchronous code are callbacks and promises.
+// - Async/await is, like promises, non blocking.
+// - Async/await is actually built on top of promises
+// - It cannot be used with plain callbacks or node callbacks.
+// - Async/await makes asynchronous code look and behave a little more like synchronous code. This is where all its power lies.
+//
+// Any async function returns a promise implicitly, and the resolve value of the promise will be whatever you return from the function (which is the string "done" in our case)
 //
 */
 
--- day-04-async-await.js
+++ day-04-async-await.js
@@ -1,1 +1,33 @@
-//async await
+// Async await
+
+// Error Handling
+
+// Promise wiht try-catch
+const makeRequest = () => {
+try {
+    getJSON()
+      .then(result => {
+        // this parse may fail
+        const data = JSON.parse(result)
+        console.log(data)
+      })
+      // uncomment this block to handle asynchronous errors
+      // .catch((err) => {
+      //   console.log(err)
+      // })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+// With Async-await
+
+const makeRequest = async () => {
+    try {
+        // this parse may fail
+        const data = JSON.parse(await getJSON())
+        console.log(data);
+    } catch (err) {
+        console.log(err);
+    }
+}
