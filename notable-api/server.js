403416daa7cbac4a896033f5f5845a957f0c207e
Parvez Kose <flamboyanz@gmail.com>
2018-01-14 19:36:09 -0500

    add server

---
+++ notable-api/server.js
@@ -0,0 +1,16 @@
+import { request } from 'http';
+
+//server.js
+
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+
+const app = express();
+
+/// port and listening
+const port = 8000;
+
+app.listen(port, () => {
+    console.log('We are live on ' + port);
+})
